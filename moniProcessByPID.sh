5bb638cae935a799be6a8add1d751ecf0c7b59d0
felix <33673540@qq.com>
2014-03-20 10:49:21 +0800

    moniProcessByPID.sh
    通过PID来判断进程是否存在，不存在则重新启动进程

---
+++ moniProcessByPID.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#通过PID来判断进程是否存在，不存在则重新启动进程
+#下面以sphinx为例子讲解
+source  $HOME/.bash_profile
+pidFile=$0
+function doStart()
+{
+	echo "echo star sphinx"
+	cd /home/uads/htdocs/sphinx/
+	sh start.sh #start.sh启动脚本中要保存进程PID
+}
+if [ ! -f $pidFile ];then
+	pid=`head -1 $pidFile`
+	kill -0 $pid
+	status=`echo $?`
+	if [ status -eq 0 ];then
+		echo "sphinx service is ok, pid is: $pid"
+	fi
+else
+	doStart
+fi
